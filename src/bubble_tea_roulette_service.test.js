e0a19606effa1197c0b85bc5eff264e3ac40a53f
Paul.Dempsey <paul.daniel.dempsey@icloud.com>
2022-03-10 08:40:31 +0000

    Stub change (mock exercise4) - new not-random tea

--- src/bubble_tea_roulette_service.test.js
+++ src/bubble_tea_roulette_service.test.js
@@ -2,15 +2,16 @@ const getRandomBubbleTeaType = require('./bubble_tea_roulette_service');
 
 test('should generate random bubble tea', () => {
   // Arrange
-  // Stub to calls to Math.random to return 0.2
-  jest.spyOn(global.Math, 'random').mockReturnValue(0.2);
+  // Stub to calls to Math.random to return 0.6
+  jest.spyOn(global.Math, 'random').mockReturnValue(0.6);
 
   // Act
   const bubbleTea = getRandomBubbleTeaType();
 
   // Assert
-  expect(bubbleTea).toBe('JASMINEMILKTEA');
+  expect(bubbleTea).toBe('PEACHICETEA');
 
   // Restore the default Math.random
   jest.spyOn(global.Math, 'random').mockRestore();
 });
+
